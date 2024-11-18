fa533b5b66097975865f066436e7b3062292a08f
MarcoBalc <marcomoisesbaltazarcabezas@gmail.com>
2024-11-18 10:13:19 -0500

    Realizar ejercicio3

---
+++ ejercicios/ejercicio3.py
@@ -0,0 +1,11 @@
+'''Escribe un programa que lea un número entero y determine si es
+positivo, negativo o cero.'''
+numero =int(input("Ingresa un numero entero: "))
+
+if numero>0:
+ print("el numero",numero,"es positivo")
+elif numero<0:
+ print("el numero",numero,"es negativo")
+else:
+     "ingresar un numero invalido"
+ 
